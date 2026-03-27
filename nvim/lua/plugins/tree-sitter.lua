15403534b97abfb127309d8024917acc6fed0dc7
ToniMaunde <maundemilton@gmail.com>
2026-03-27 17:16:09 +0200

    fix(nvim-treesitter): update and change configuration

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -1,11 +1,36 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
+		local treesitter = require("nvim-treesitter")
+		treesitter.setup()
+		treesitter.install({
+			"c",
+			"cpp",
+			"lua",
+			"vim",
+			"vimdoc",
+			"typescript",
+			"javascript",
+			"html",
+			"css",
+			"astro",
+			"go",
+			"json",
+			"markdown",
+			"markdown_inline",
+			"sql",
+			"svelte",
+			"yaml",
+			"xml",
+			"rust",
+			"nginx",
+			"bash",
+		})
 
-		configs.setup({
-			ensure_installed = {
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
 				"c",
 				"cpp",
 				"lua",
@@ -28,21 +53,10 @@ return {
 				"nginx",
 				"bash",
 			},
-			auto_install = false,
-			ignore_install = {},
-			sync_install = false,
-			modules = {},
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<Enter>", -- set to `false` to disable one of the mappings
-					node_incremental = "<Enter>",
-					scope_incremental = false,
-					node_decremental = "<Backspace>",
-				},
-			},
+			callback = function()
+				vim.treesitter.start()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end,
 }
